60a0247825b3ec5998e468563e9dfb2c8716c012
RONGEARD Benoit <benoit.rongeard@coexya.eu>
2022-06-20 12:00:07 +0200

    Reset app + TU

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,27 +8,57 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: "Phone XL",
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: "A large phone with one of the best screens",
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: "Phone Mini",
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: "A great phone with one of the best cameras",
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: "Phone Standard",
     price: 299,
-    description: ''
-  }
+    description: "",
+  },
+  // generate products
+  {
+    id: 4,
+    name: "Phone XL",
+    price: 799,
+    description: "A large phone with one of the best screens",
+  },
+  {
+    id: 5,
+    name: "Phone Mini",
+    price: 699,
+    description: "A great phone with one of the best cameras",
+  },
+  {
+    id: 6,
+    name: "Phone Standard",
+    price: 299,
+    description: "",
+  },
+  {
+    id: 7,
+    name: "Phone XL",
+    price: 799,
+    description: "A large phone with one of the best screens",
+  },
+  {
+    id: 8,
+    name: "Phone Mini",
+    price: 699,
+    description: "A great phone with one of the best cameras",
+  },
+  {
+    id: 9,
+    name: "Phone Standard",
+    price: 299,
+    description: "",
+  },
 ];
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
